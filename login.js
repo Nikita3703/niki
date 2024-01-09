e2e88227ca6ce73e2280f9a1bb737f34bca8b7a4
ニキータ・パリク <90152301+Nikita3703@users.noreply.github.com>
2024-01-09 18:05:33 +0530

    Delete login.js

--- login.js
+++
@@ -1,7 +0,0 @@
-function onSignIn(googleUser) {
-    // Get user information
-    var profile = googleUser.getBasicProfile();
-
-    // Display user information
-    document.getElementById('result').innerHTML = 'Welcome, ' + profile.getName();
-}
